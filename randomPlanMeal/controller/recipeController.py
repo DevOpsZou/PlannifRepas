ab991c5499b6f3411666de1fbaa6a00d9936535e
zoubida afoutni <afoutniz@gmail.com>
2022-01-07 15:26:09 +0100

    ajout entree et plat

--- randomPlanMeal/controller/recipeController.py
+++ randomPlanMeal/controller/recipeController.py
@@ -9,14 +9,14 @@ class RecipeController():
         
 
     def read(self, recipe):
-        print("JE suis la")
+        
         result = recipe.read()
         
         return render_template("displayPlanWeek.html", data= result, len=len(result))
         
     def readRecipe(self, recipe, id):
-        print("JE suis la")
+       
         result = recipe.readRecipe(id)
-        print(result)
+       
         return render_template("displayRecipe.html", data= result, len=len(result))
         
