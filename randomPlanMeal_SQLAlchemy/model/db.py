222334fadfeddea9843a9a09462c955ac7a1b206
zoubida afoutni <afoutniz@gmail.com>
2022-01-14 14:26:55 +0100

    Create randomPlanRecipe with SQLAlchemy

---
+++ randomPlanMeal_SQLAlchemy/model/db.py
@@ -0,0 +1,63 @@
+
+import os
+from dotenv import load_dotenv
+
+from sqlalchemy.orm import sessionmaker, Session, load_only
+import pymysql
+from sqlalchemy.ext.automap import automap_base
+
+from sqlalchemy import create_engine, MetaData, Table, Column
+from sqlalchemy.orm import defer
+from sqlalchemy.orm import undefer
+from  sqlalchemy.sql.expression import func
+
+'''
+Authors : Zoubida AFOUTNI
+Date : 13/01/2022
+'''
+class Db():
+    
+    def __init__(self, value=load_dotenv()):
+      
+        self.url=value
+        self.logging()
+      
+    @property
+    def url(self):
+        return self._url
+ 
+    @url.setter
+    def url(self, value):
+        self._url = "%s://%s:%s@%s:%s/%s" % (
+        os.getenv("DRIVER"),
+        os.getenv("USER"),
+        os.getenv("PASSWORD"),
+        os.getenv("HOST"),
+        os.getenv("PORT"),
+        os.getenv('DATABASE')
+    )
+   
+
+    def logging(self):
+        """[contains code to connect to database : recettebis]
+        """
+     
+        self.engine = create_engine(self.url)
+        self.DBSession = sessionmaker(bind=self.engine)
+        self.session = self.DBSession()
+        self.metadata = MetaData()
+        self.metadata.reflect(self.engine)
+        self.Base = automap_base(metadata=self.metadata)
+        self.metadata.reflect(self.engine)
+        self.Base.prepare()
+        self.Recette = self.Base.classes.recette
+        self.Categorie = self.Base.classes.categorie
+        self.Recette_ingredient=self.Base.classes.recette_ingredients
+        self.Ingredient=self.Base.classes.ingredient
+        self.Etapes=self.Base.classes.etapes
+            
+    
+ 
+     
+
+
